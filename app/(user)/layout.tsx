e29094b453195b1bb949c726d539cf7a9470fdab
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2023-01-29 00:13:38 -0500

    feat: add banner

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,5 +1,6 @@
 import Header from '@/components/Header';
 import '@/app/globals.css';
+import Banner from '@/components/Banner';
 
 export default function RootLayout({
   children,
@@ -11,6 +12,7 @@ export default function RootLayout({
       <head />
       <body>
         <Header />
+        <Banner />
         {children}
       </body>
     </html>
---
+++ components/Banner.tsx
@@ -0,0 +1,23 @@
+export default function Banner() {
+  return (
+    <div className='flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10'>
+      <div>
+        <h1 className='text-7xl'>
+          {'Educación Infantil | Blog by Nayade Novellis'}
+        </h1>
+        <h2 className='mt-5 md:mt-0'>
+          {'Bienvenidos a '}
+          <span className='underline decoration-4 decoration-[#F7AB0A]'>
+            cada lector
+          </span>
+          {' al maravilloso mundo de la educación en la primera infancia'}
+        </h2>
+      </div>
+      <p className='mt-5 md:mt-2 text-gray-400 max-w-sm'>
+        {
+          'Desarrollo cognitivo | Desarrollo emocional/social | Desarrollo del habla/lenguaje | Habilidades motoras'
+        }
+      </p>
+    </div>
+  );
+}
